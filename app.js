2fde7a5ec45e217271595bc74f86dda5ada44d3d
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-29 11:38:22 +0700

    js lab 157

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// write code here
+// ผลลัพธ์ในบรรทัดที่มี * มีค่าเป็นอะไรและเพราะอะไร
+
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]; //เป็นการใช้ array destructuring อ้างอิงค่าใน  numbers จากนั้นทำการสลับค่าของ indexที่ 1 กับ 2
+console.log(numbers); // [ 10, 30, 20 ]
